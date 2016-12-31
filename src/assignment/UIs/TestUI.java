5bbdcff9792840a696a3fc908844ebcd0987b1b4
Roledene <sroledenez@gmail.com>
2016-12-31 20:42:49 +0530

    UI created

--- src/assignment/UIs/TestUI.java
+++ src/assignment/UIs/TestUI.java
@@ -6,9 +6,14 @@
 package assignment.UIs;
 
 import assignment.database.UserService;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -16,6 +21,8 @@ import javax.swing.event.ListSelectionListener;
  */
 public class TestUI extends javax.swing.JFrame {
 
+    DefaultTableModel tableModel = new DefaultTableModel();
+    ListSelectionModel model;
     /**
      * Creates new form TestUI
      */
@@ -27,14 +34,33 @@ public class TestUI extends javax.swing.JFrame {
             System.out.println(e.getMessage());
         }
 
-        ListSelectionModel model = testTable.getSelectionModel();
-        model.addListSelectionListener(new ListSelectionListener() {
+        model = testTable.getSelectionModel();
+        tableModel = (DefaultTableModel) testTable.getModel();
+//        
+//        model.addListSelectionListener(new ListSelectionListener() {
+//            @Override
+//            public void valueChanged(ListSelectionEvent e) {
+//                if (!model.isSelectionEmpty()) {
+//                    int selectedRow = model.getMinSelectionIndex();
+//
+//                    System.out.println("Selected Raw : " + selectedRow);
+//                }
+//            }
+//        });
+
+        updateBtn.addActionListener(new ActionListener() {
             @Override
-            public void valueChanged(ListSelectionEvent e) {
-                if (!model.isSelectionEmpty()) {
-                    int selectedRow = model.getMinSelectionIndex();
-                    System.out.println("Selected Raw : "+selectedRow);
-                }
+            public void actionPerformed(ActionEvent e) {
+                int i = testTable.getSelectedRow();
+
+            }
+        });
+
+        testTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                int i = testTable.getSelectedRow();
+                System.out.println(tableModel.getValueAt(i, 0));
             }
         });
     }
@@ -50,6 +76,7 @@ public class TestUI extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         testTable = new javax.swing.JTable();
+        updateBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,25 +93,43 @@ public class TestUI extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(testTable);
 
+        updateBtn.setText("Update");
+        updateBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateBtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(150, 150, 150)
+                        .addComponent(updateBtn)))
                 .addContainerGap(15, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(14, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(90, 90, 90))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(55, 55, 55)
+                .addComponent(updateBtn)
+                .addGap(67, 67, 67))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void updateBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_updateBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,5 +169,6 @@ public class TestUI extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable testTable;
+    private javax.swing.JButton updateBtn;
     // End of variables declaration//GEN-END:variables
 }
